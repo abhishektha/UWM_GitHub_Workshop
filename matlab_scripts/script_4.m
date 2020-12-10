4e01ce49ebe58ec91968a62a40b3afdb0956208d
Yu Lu <sherryaseity@gmail.com>
2020-12-10 17:22:11 -0600

    script_3.m

--- matlab_scripts/script_4.m
+++ matlab_scripts/script_4.m
@@ -4,15 +4,15 @@
 %
 
 %read in data
-data = csvread('data/lower_banner.csv');
+%data = csvread('data/lower_banner.csv');
 %plot data
-l = patch(data(:,1), data(:,2), 'black');
-l.LineWidth = 2;
+%l = patch(data(:,1), data(:,2), 'black');
+%l.LineWidth = 2;
 
 %% SOLUTION: Breakout 1
 
-% %read in data
-% data = csvread('data/lower_banner.csv');
-% %plot data
-% l = patch(data(:,1), data(:,2), [0, 0.2, 0.4]);  % per reviewer comment
-% l.LineWidth = 2;
+%read in data
+data = csvread('data/lower_banner.csv');
+%plot data
+l = patch(data(:,1), data(:,2), [0, 0.2, 0.4]);  % per reviewer comment
+l.LineWidth = 2;
